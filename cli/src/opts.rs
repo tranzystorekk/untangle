0a32dc4a7003cb53985171b918c09f327e413ca5
Marcin Puc <marcin.e.puc@gmail.com>
2020-08-23 18:57:28 +0200

    feat(cli): add input method to Opts

--- cli/src/opts.rs
+++ cli/src/opts.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
 use structopt::StructOpt;
@@ -7,4 +9,22 @@ use structopt::StructOpt;
 pub struct Opts {
     #[structopt(parse(from_os_str))]
     input: Option<PathBuf>,
+}
+
+impl Opts {
+    pub fn input(&self) -> std::io::Result<impl BufRead> {
+        let result: Box<dyn BufRead> = match self.input.as_ref() {
+            Some(path) => {
+                let file = File::open(path)?;
+                let reader = BufReader::new(file);
+                Box::new(reader)
+            },
+            _ => {
+                let reader = BufReader::new(std::io::stdin());
+                Box::new(reader)
+            },
+        };
+
+        Ok(result)
+    }
 }
